5fcadab2b6c907a1133276630b0634bcd9fa0d42
zainMahar78 <kahansuno3@gmail.com>
2025-12-06 14:48:04 +0500

    final commit on 2:47 PM 6/12/2025

--- script.js
+++ script.js
@@ -2,23 +2,44 @@ const input = document.getElementById("input");
 const btn = document.getElementById("searchBtn");
 const divToDisplayTemp = document.getElementById("divToDisplayTemp");
 btn.addEventListener("click", showTemp);
-function showTemp(){
-
+function showTemp() {
+    if (input.value === "") {
+        divToDisplayTemp.innerHTML = "";
+        const divForAlert = document.createElement("div");
+        divForAlert.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show");
+        divForAlert.role = "alert";
+        divForAlert.id = "alert";
+        divForAlert.innerHTML = `<strong>Holy guacamole!</strong> Enter the name of the city.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+`;
+        divToDisplayTemp.appendChild(divForAlert);
+        return;
+    }
     const Url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=a7206100fc1c785dbd84874cc0d6ba57`;
     let tempInC;
-    fetch(Url).then((response)=>{
-        
+    fetch(Url).then((response) => {
+
         return response.json()
     }).then(data => {
-        const img = Image(data.weather[0].icon)
+        divToDisplayTemp.innerHTML = "";
+        const divForCity = document.createElement("div");
+        divForCity.innerHTML = `<p><b>${data.name}</b></p>`;
+        divToDisplayTemp.appendChild(divForCity);
+        const divForImg = document.createElement("div");
+        divForImg.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+        divToDisplayTemp.appendChild(divForImg);
+        tempInC = Math.round(data.main.temp - 273.15);
+        const divForPara = document.createElement("div");
+        divForPara.innerHTML = `        <p class="temp">Temperature: ${tempInC}&deg;C</p>`;
+        divToDisplayTemp.appendChild(divForPara);
         console.log(data);
-         tempInC = Math.round(data.main.temp - 273.15);
+        console.log(data.name);
         console.log(data.weather[0].icon);
-        // divToDisplayTemp.innerText = "";
-        divToDisplayTemp.innerText =`Temperature: ${tempInC}`;
-    
-    }).catch(err => console.log(err));
 
-    
 
+    }).catch(err => {
+        console.log(err)
+        divToDisplayTemp.innerHTML = "<p>City not found</p>";
+    }
+    );
 }
